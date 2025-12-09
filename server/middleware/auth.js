38e5aee778577bca4df39dfb98a2d91d2dec849f
Kevin L <kevinliu20210000@gmail.com>
2025-12-08 20:21:07 -0600

    update authcheck

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -12,8 +12,8 @@ export const protectRoute = async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findById(decoded.userId).select("-password");
 
-        if (!user) return res.status(401).json({ success: false, message: 'User not found' })
-        
+        if (!user) return res.json({ success: false, message: 'User not found' })
+
         req.user = user;
         next();
     } catch (error) {
